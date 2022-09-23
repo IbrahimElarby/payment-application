7cf226c5c912e39540e765b799a065e49fec5445
IbrahimElarby <92256356+IbrahimElarby@users.noreply.github.com>
2022-09-23 22:19:57 +0200

    Update helper.h

--- udacity/helper function/helper.h
+++ udacity/helper function/helper.h
@@ -2,12 +2,14 @@
 #define helper_h
 
 #include"../Server/Server.h"
-void intialize_table();
-void intialize_table_transaction();
-int hashfunction(char* str, int size);
+
+void copystrr(unsigned char* old , unsigned char* neww);
+
+
+int hashfunction(uint8_t* str, uint32_t size);
 void insert_hashtable(ST_accountsDB_t member);
-int lookup_table(char* pan);
-ST_accountsDB_t* lookup_table1(char* pan);
-ST_transaction_t* binarysearch(int transactionNumber, int a, int b);
+int lookup_table(uint8_t* pan);
+int lookup_table1(uint8_t* pan);
+int binarysearch(uint32_t transactionNumber, uint32_t a, uint32_t b);
 #endif // !helper.h
 
