83e8677e87228412da9e2cac35f33eca6428f107
IbrahimElarby <92256356+IbrahimElarby@users.noreply.github.com>
2022-09-23 22:16:58 +0200

    Update Server.h

--- udacity/Server/Server.h
+++ udacity/Server/Server.h
@@ -4,6 +4,9 @@
 #include"../Terminal/Terminal.h"
 
 
+typedef unsigned int uint32_t;
+
+
 typedef enum EN_transState_t
 {
 	APPROVED, DECLINED_INSUFFECIENT_FUND, DECLINED_STOLEN_CARD, FRAUD_CARD, INTERNAL_SERVER_ERROR
@@ -14,7 +17,7 @@ typedef struct ST_transaction_t
 	ST_cardData_t cardHolderData;
 	ST_terminalData_t terminalData;
 	EN_transState_t transState;
-	int transactionSequenceNumber;
+	uint32_t transactionSequenceNumber;
 }ST_transaction_t;
 
 typedef enum EN_serverError_t
@@ -32,16 +35,17 @@ typedef struct ST_accountsDB_t
 {
 	float balance;
 	EN_accountState_t state;
-	char primaryAccountNumber[20];
+	uint8_t primaryAccountNumber[20];
 }ST_accountsDB_t;
 
  ST_accountsDB_t account_database[255];
-  ST_transaction_t side_transaction_database[255];
+ ST_transaction_t side_transaction_database[255];
 
 EN_transState_t recieveTransactionData(ST_transaction_t* transData);
 EN_serverError_t isValidAccount(ST_cardData_t* cardData, ST_accountsDB_t* accountRefrence);
 EN_serverError_t isAmountAvailable(ST_terminalData_t* termData, ST_accountsDB_t* accountRefrence);
 EN_serverError_t saveTransaction(ST_transaction_t* transData);
-EN_serverError_t getTransaction(int transactionSequenceNumber, ST_transaction_t* transData);
+EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t* transData);
 
 #endif // !Server.h
+
