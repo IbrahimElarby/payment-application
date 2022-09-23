6a89b5e811a32fd171e96ab54cccacc8c94bdcd5
IbrahimElarby <92256356+IbrahimElarby@users.noreply.github.com>
2022-09-23 22:17:48 +0200

    Update Server.c

--- udacity/Server/Server.c
+++ udacity/Server/Server.c
@@ -7,29 +7,47 @@
 
 
 
-ST_accountsDB_t person = { 2000.0 ,RUNNING,"8989374615436851"};
-void insert_hashtable(person);
+
 EN_transState_t recieveTransactionData(ST_transaction_t* transData)
 {
-	ST_accountsDB_t* member = lookup_table1(transData->cardHolderData.primaryAccountNumber);
-	if (lookup_table(transData->cardHolderData.primaryAccountNumber)) return FRAUD_CARD;
-	if (transData->terminalData.transAmount > member->balance) return DECLINED_INSUFFECIENT_FUND;
-	if (member->state == BLOCKED) return DECLINED_STOLEN_CARD;
+    int index = lookup_table1(transData->cardHolderData.primaryAccountNumber);
+	ST_accountsDB_t * member = &account_database[index] ;
+
+	if (lookup_table(transData->cardHolderData.primaryAccountNumber))
+    {printf("fraud card\n");
+        return FRAUD_CARD;}
+	if (transData->terminalData.transAmount > member->balance)
+    {printf("insuffecient fund\n");
+        return DECLINED_INSUFFECIENT_FUND;}
+	if (member->state == BLOCKED)
+    { printf("decline stolen card\n");
+        return DECLINED_STOLEN_CARD;}
 	for (int i = 0; i < 255; i++)
 	{
-		if (side_transaction_database[i].transState == NULL)
+		if (side_transaction_database[i].transactionSequenceNumber == 0)
 		{
 			side_transaction_database[i] = *transData;
+			printf("your account was  %f  \n",member->balance);
 			member->balance = member->balance - transData->terminalData.transAmount;
+			int index = hashfunction(transData->cardHolderData.primaryAccountNumber,255);
+			//account_database[index].balance = member->balance;
+			printf("now is %f \n",account_database[index].balance);
+			printf("successful operation \n");
+
 			return APPROVED;
 		}
 	}
+	printf("internal server error");
 	return INTERNAL_SERVER_ERROR;
 }
 EN_serverError_t isValidAccount(ST_cardData_t* cardData, ST_accountsDB_t* accountRefrence)
 {
-	accountRefrence = lookup_table1(cardData->primaryAccountNumber);
-	if (accountRefrence == NULL) return ACCOUNT_NOT_FOUND;
+
+	int index = lookup_table1(cardData->primaryAccountNumber);
+	//printf("index is %d\n",index);
+	*accountRefrence = account_database[index];
+	//printf("%f   %s \n  %f   %s   ", accountRefrence->balance, accountRefrence->primaryAccountNumber, account_database[index].balance , account_database[index].primaryAccountNumber);
+	if (accountRefrence->primaryAccountNumber[0] == 0) return ACCOUNT_NOT_FOUND;
 	return SERVER_OK;
 }
 EN_serverError_t isAmountAvailable(ST_terminalData_t* termData, ST_accountsDB_t* accountRefrence)
@@ -41,7 +59,7 @@ EN_serverError_t saveTransaction(ST_transaction_t* transData)
 {
 	for (int i = 0; i < 255; i++)
 	{
-		if (side_transaction_database[i].transState == NULL)
+		if (side_transaction_database[i].transactionSequenceNumber == 0)
 		{
 			side_transaction_database[i] = *transData;
 			if (i != 0)
@@ -53,8 +71,8 @@ EN_serverError_t saveTransaction(ST_transaction_t* transData)
 	}
 	return SAVING_FAILED;
 }
-EN_serverError_t getTransaction(int transactionSequenceNumber, ST_transaction_t* transData)
+EN_serverError_t getTransaction(uint32_t transactionSequenceNumber, ST_transaction_t* transData)
 {
-	if (binarysearch(transactionSequenceNumber, 0, 255) == NULL) return TRANSACTION_NOT_FOUND;
+	if (binarysearch(transactionSequenceNumber, 0, 255) == -1) return TRANSACTION_NOT_FOUND;
 	return SERVER_OK;
 }
