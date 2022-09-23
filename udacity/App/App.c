faf7834947d5966cf0bb11be31af18a87f21feb9
IbrahimElarby <92256356+IbrahimElarby@users.noreply.github.com>
2022-09-23 22:19:06 +0200

    Update App.c

--- udacity/App/App.c
+++ udacity/App/App.c
@@ -1,53 +1,51 @@
 #include"App.h"
 #include"../helper function/helper.h"
 #include"../Server/Server.h"
-#include"../Terminal/Terminal.h"
-#include"../Card/Card.h"
 #include<stdio.h>
 #include<stdlib.h>
-ST_cardData_t mycard;
-ST_terminalData_t mytransaction;
+ST_transaction_t mytransaction;
 ST_accountsDB_t myaccount ;
+
 void appStart(void)
 {
-	if(getCardHolderName(&mycard) == WRONG_NAME)
+	if(getCardHolderName(&(mytransaction.cardHolderData)) == WRONG_NAME)
     {
         printf("WORNG NAME FORMAT");
         return;
     }
-	if(getCardExpiryDate(&mycard) == WRONG_EXP_DATE)
+	if(getCardExpiryDate(&(mytransaction.cardHolderData)) == WRONG_EXP_DATE)
     {
         printf("WRONG EXP_DATE FORMAT");
 	    return ;
     }
-    if(getCardPAN(&mycard)== WRONG_PAN)
+    if(getCardPAN(&(mytransaction.cardHolderData))== WRONG_PAN)
     {
         printf("WRONG PAN FORMAT");
 	    return ;
     }
-	if( getTransactionDate(&mytransaction) == WRONG_DATE)
+	if( getTransactionDate(&(mytransaction.terminalData)) == WRONG_DATE)
     {
         printf("WRONG DATE FORMAT");
         return;
     }
-	if (isCardExpired(mycard, mytransaction) == EXPIRED_CARD)
+	if (isCardExpired(&(mytransaction.cardHolderData),&(mytransaction.terminalData)) == EXPIRED_CARD)
 	{
 		printf("EXPIRED CARD\n");
 		return;
 	}
-	getTransactionAmount(&mytransaction);
-	setMaxAmount(&mytransaction);
-	if (isBelowMaxAmount(&mytransaction) == EXCEED_MAX_AMOUNT)
+	getTransactionAmount(&(mytransaction.terminalData));
+	setMaxAmount(&(mytransaction.terminalData));
+	if (isBelowMaxAmount(&(mytransaction.terminalData)) == EXCEED_MAX_AMOUNT)
 	{
 		printf("EXCEEDED MAX AMOUNT");
 		return;
 	}
-	if (isValidAccount(&mycard, &myaccount) == ACCOUNT_NOT_FOUND)
+	if (isValidAccount(&(mytransaction.cardHolderData), &myaccount) == ACCOUNT_NOT_FOUND)
 	{
 		printf("ACCOUNT NOT FOUND");
 		return;
 	}
-	if (isAmountAvailable(&mytransaction, &myaccount) == LOW_BALANCE)
+	if (isAmountAvailable(&(mytransaction.terminalData), &myaccount) == LOW_BALANCE)
 	{
 		printf("LOW BALANCE");
 		return;
